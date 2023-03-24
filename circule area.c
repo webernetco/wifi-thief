6a7034582223e18d59fad26d3ff13dd35b013262
Shalitha Madhuwantha <78067281+shalithamadhuwantha@users.noreply.github.com>
2023-03-24 15:05:16 +0530

    Add files via upload

---
+++ circule area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(){
+    const float PI = 3.14;
+    float r;
+    printf("ENTER THE RADIUS: ");
+    scanf("%f",&r);
+    printf("your circle area is %0.2f",(PI*(r*r)));
+
+
+}
---
+++ wincmd.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main(){
+    printf("start");
+    system("ipconfig");
+    printf("\n");
+    system("whoami");
+}
